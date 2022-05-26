ac3328b864cb0c5b47a33d35d62ff42709a2058a
Philipp Lemke <P.lemke91@gmail.com>
2022-05-26 22:17:24 +0200

    Fehlerbehebung Image Controller

--- display_controller.py
+++ display_controller.py
@@ -12,6 +12,7 @@ class DisplayController:
     def update_display(self):
         DisplayController.__controller.init(DisplayController.__controller.lut_full_update)
         self.__controller.Clear(0xFF)
+        self.__image_controller.close()
         self.__controller.display(self.__controller.getbuffer(self.__image_controller))
 
     def add_text_to_frame(self, text: str, coordinates: tuple, font_size=EPaperUtil.font10):
@@ -24,5 +25,6 @@ class DisplayController:
         self.__draw_controller.line(x1y1x2y2)
 
     def clear_display(self):
+        self.__image_controller.close()
         self.__controller.Clear(0xFF)
 
